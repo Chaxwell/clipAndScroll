2162bad9337ea70d49b3295d4db7f5b93b89c410
vozenilek <vozenilekaxel@gmail.com>
2019-05-06 16:07:47 +0200

    header

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-hello, world.
-Coucou c'est Axel
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    include 'partials/header.php'
+    ?>
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,39 @@
+<?php
+if (!isset($_SESSION["userId"])) {
+    
+    echo '
+   
+    <header>
+    
+    <nav>
+    <div class="logo"></div>
+      connecter
+    <h1 class="connecte-titre">clipAndScroll
+    </h1>
+    <div class="logo"><img class="logo" src="https://www.mtsac.edu/asac/images/temp_logo_testing.png"></img></div>
+
+<a class="upload">UPLOAD</a>
+    </nav>
+    </header>
+    
+    ';
+}
+if (isset($_SESSION["userId"])) {
+    
+
+    echo '
+   
+    <header>
+    
+    <nav>
+    <div class="logo"><img class="logo" src="https://www.mtsac.edu/asac/images/temp_logo_testing.png"></img></div>
+     pas connecter
+    <h1>clipAndScroll
+    </h1>
+    
+    </nav>
+    </header>
+    
+    ';
+    }
+?> 
