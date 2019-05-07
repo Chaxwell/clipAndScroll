f2ccd1d82fd3bd31b4aff92d69c85f693c3776ac
HeathSilcox <chaxel.christophe@gmail.com>
2019-05-07 16:59:30 +0200

    Ajout page upload et traitements Correction bug connexion

---
+++ processing/traitement-upload.php
@@ -0,0 +1,6 @@
+<?php
+
+// We check if the globals aren't empty
+if (!empty($_POST['clipFile']) && !empty($_POST['clipThumbnail']) && !empty($_POST['clipName']) && !empty($_POST['clipDescription'])) {
+    // Traitement
+}
