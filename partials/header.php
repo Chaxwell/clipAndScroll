7e0ecf5b322fd44ca702e82f4a61c15d00980ea8
vozenilek <vozenilekaxel@gmail.com>
2019-05-07 16:45:49 +0200

    header resou scss

--- partials/header.php
+++ partials/header.php
@@ -6,7 +6,7 @@ if (isset($_SESSION["userId"])) {
    
     <header>
         <div class="logo">
-          <img class="logo user-dropdown" src="https://www.mtsac.edu/asac/images/temp_logo_testing.png">
+          <a href="/index.php"><img class="logo" src="https://www.mtsac.edu/asac/images/temp_logo_testing.png"></a>
         </div>
 
         <h1 class="connecte-titre">ClipAndScroll</h1>
@@ -14,7 +14,9 @@ if (isset($_SESSION["userId"])) {
         <nav>
            <a class="upload" href="../pages/clip-upload.php">upload</a>
                        <a class="user-dropdown" href="#">' . $_SESSION['nickname'] . '</a> 
-
+  <div class="user-main">
+  <a href="/partials/processing/deconnexion.php">déconnexion</a>
+  </div>
            
        </nav>
     </header>
@@ -29,7 +31,9 @@ if (!isset($_SESSION['userId'])) {
      <div class="logo">
              <img class="logo" src="https://www.mtsac.edu/asac/images/temp_logo_testing.png">
         </div>
-        <h1>ClipAndScroll</h1>
+
+        <h1 class="titre-pas-co">ClipAndScroll</h1>
+        
         <nav>
         ';
   require($path . "/partials/connexion-inscription.php");
