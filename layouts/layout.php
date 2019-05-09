1f525defcd1dc75dded34c05dc8e824465204c84
HeathSilcox <chaxel.christophe@gmail.com>
2019-05-09 12:33:05 +0200

    adding CDN instead of local files for popper.js

--- layouts/layout.php
+++ layouts/layout.php
@@ -25,7 +25,7 @@ $path = $_SERVER['DOCUMENT_ROOT'];
 
     <script src=""></script>
     <script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
-    <script src="../assets/vendor/popper.js/dist/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="../assets/vendor/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
     <script src="../assets/js/script.js"></script>
 </body>
